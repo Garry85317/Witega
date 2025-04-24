82a3324383ccedec07e78a05497a5abd5db21c84
Garry <>
2025-04-24 15:18:48 +0800

    fix: 修正router 及新增產品列表頁面

---
+++ assets/data/products.js
@@ -0,0 +1,262 @@
+const productsData = {
+  "categories": [
+    {
+      "id": "tools",
+      "name": "省工機具",
+      "products": [
+        {
+          "id": "K1",
+          "name": "電動公豬試情車",
+          "img": "assets/img/products/K1/K1-1.jpg",
+          "url": "product/K1.html"
+        },
+        {
+          "id": "T1",
+          "name": "多功能斃死豬搬運車",
+          "img": "assets/img/products/T1/T1-2.jpg",
+          "url": "product/T1.html"
+        },
+        {
+          "id": "R1",
+          "name": "電動小豬搬運車",
+          "img": "assets/img/products/R1/R1-1.png",
+          "url": "product/R1.html"
+        },
+        {
+          "id": "F1",
+          "name": "多功能仔豬工作車",
+          "img": "assets/img/products/F1/F1-1.jpg",
+          "url": "product/F1.html"
+        },
+        {
+          "id": "EPIG",
+          "name": "EPIG 無針注射器",
+          "img": "assets/img/products/EPIG/EPIG-1.jpg",
+          "url": "product/EPIG.html"
+        },
+        {
+          "id": "tooth",
+          "name": "電動磨牙機",
+          "img": "assets/img/products/tooth/tooth-1.jpg",
+          "url": "product/tooth.html"
+        }
+      ]
+    },
+    {
+      "id": "smart-detection",
+      "name": "智能檢測儀器",
+      "products": [
+        {
+          "id": "RX-1",
+          "name": "無線超音波測孕器",
+          "img": "assets/img/products/RX1/RX1-4.png",
+          "url": "product/RX-1.html"
+        },
+        {
+          "id": "RU-1",
+          "name": "無線超音波背脂機",
+          "img": "assets/img/products/RU1/RU1-2.jpg",
+          "url": "product/RU-1.html"
+        },
+        {
+          "id": "RL-1",
+          "name": "無線超音波腰眼面積測定機",
+          "img": "assets/img/products/RL1/RL1-2.jpg",
+          "url": "product/RL-1.html"
+        },
+        {
+          "id": "G1",
+          "name": "精子相位差顯微鏡",
+          "img": "assets/img/products/G1/G1-1.jpg",
+          "url": "product/G1.html"
+        },
+        {
+          "id": "iSperm",
+          "name": "自動精子檢測機",
+          "img": "assets/img/products/iSperm/iSperm-1.jpg",
+          "url": "product/iSperm.html"
+        },
+        {
+          "id": "ammoniaDetector",
+          "name": "氨氣檢測儀",
+          "img": "assets/img/products/smartDetection/ammoniaDetector.jpg",
+          "url": "product/smartDetection/ammoniaDetector.html"
+        },
+        {
+          "id": "anemometer",
+          "name": "風速風向儀",
+          "img": "assets/img/products/smartDetection/anemometer.jpg",
+          "url": "product/smartDetection/anemometer.html"
+        },
+        {
+          "id": "animalThermometer",
+          "name": "獸用體溫計",
+          "img": "assets/img/products/smartDetection/animalThermometer.jpg",
+          "url": "product/smartDetection/animalThermometer.html"
+        },
+        {
+          "id": "digitalThermo",
+          "name": "電子溫度計",
+          "img": "assets/img/products/smartDetection/digitalThermo.jpg",
+          "url": "product/smartDetection/digitalThermo.html"
+        },
+        {
+          "id": "infraRedThermometer",
+          "name": "遠紅外溫度記錄儀",
+          "img": "assets/img/products/smartDetection/infraRedThermometer.jpg",
+          "url": "product/smartDetection/infraRedThermometer.html"
+        },
+        {
+          "id": "lightTester",
+          "name": "光照測定儀",
+          "img": "assets/img/products/smartDetection/lightTester.jpg",
+          "url": "product/smartDetection/lightTester.html"
+        },
+        {
+          "id": "maxMinThermometer",
+          "name": "高低溫度計",
+          "img": "assets/img/products/smartDetection/maxMinThermometer.jpg",
+          "url": "product/smartDetection/maxMinThermometer.html"
+        },
+        {
+          "id": "octagonThermoHygro",
+          "name": "八角指標溫濕度計",
+          "img": "assets/img/products/smartDetection/octagonThermoHygro.jpg",
+          "url": "product/smartDetection/octagonThermoHygro.html"
+        },
+        {
+          "id": "PHTestPaper",
+          "name": "PH試紙",
+          "img": "assets/img/products/smartDetection/PHTestPaper.jpg",
+          "url": "product/smartDetection/PHTestPaper.html"
+        },
+        {
+          "id": "remoteTemperatureDetector",
+          "name": "遠程溫度檢測儀S20D（彭雲物聯WiFi）",
+          "img": "assets/img/products/smartDetection/remoteTemperatureDetector.jpg",
+          "url": "product/smartDetection/remoteTemperatureDetector.html"
+        },
+        {
+          "id": "temperatureHumidityMonitor",
+          "name": "乾濕溫度記錄儀",
+          "img": "assets/img/products/smartDetection/temperatureHumidityMonitor.jpg",
+          "url": "product/smartDetection/temperatureHumidityMonitor.html"
+        },
+        {
+          "id": "temperatureHumidityProbe",
+          "name": "溫濕度記錄儀",
+          "img": "assets/img/products/smartDetection/temperatureHumidityProbe.jpg",
+          "url": "product/smartDetection/temperatureHumidityProbe.html"
+        },
+        {
+          "id": "tempRecorder",
+          "name": "溫度記錄儀",
+          "img": "assets/img/products/smartDetection/tempRecorder.jpg",
+          "url": "product/smartDetection/tempRecorder.html"
+        },
+        {
+          "id": "thermoHygroMeter",
+          "name": "乾濕溫度計",
+          "img": "assets/img/products/smartDetection/thermoHygroMeter.jpg",
+          "url": "product/smartDetection/thermoHygroMeter.html"
+        }
+      ]
+    },
+    {
+      "id": "biosecurity",
+      "name": "生物安全防治設備",
+      "products": [
+        {
+          "id": "wash",
+          "name": "高壓清洗機",
+          "img": "assets/img/products/wash/wash-1.jpg",
+          "url": "product/wash.html"
+        }
+      ]
+    },
+    {
+      "id": "animal-marking",
+      "name": "動物標示",
+      "products": [
+        {
+          "id": "animalMarkingSpray",
+          "name": "動物標記噴劑",
+          "img": "assets/img/products/animalLabels/animalMarkingSpray.jpg",
+          "url": "product/animalLabels/animalMarkingSpray.html"
+        },
+        {
+          "id": "automaticTagApplicator",
+          "name": "自動打耳標器",
+          "img": "assets/img/products/animalLabels/automaticTagApplicator.jpg",
+          "url": "product/animalLabels/automaticTagApplicator.html"
+        },
+        {
+          "id": "circleEarTag",
+          "name": "圓形耳標",
+          "img": "assets/img/products/animalLabels/circleEarTag.jpg",
+          "url": "product/animalLabels/circleEarTag.html"
+        },
+        {
+          "id": "cutterForRemovingEarTag",
+          "name": "耳標拆除器",
+          "img": "assets/img/products/animalLabels/cutterForRemovingEarTag.jpg",
+          "url": "product/animalLabels/cutterForRemovingEarTag.html"
+        },
+        {
+          "id": "earCutter",
+          "name": "耳缺刀",
+          "img": "assets/img/products/animalLabels/earCutter.jpg",
+          "url": "product/animalLabels/earCutter.html"
+        },
+        {
+          "id": "earNotcher",
+          "name": "耳缺器",
+          "img": "assets/img/products/animalLabels/earNotcher.jpg",
+          "url": "product/animalLabels/earNotcher.html"
+        },
+        {
+          "id": "earTagMarkerPen",
+          "name": "耳標及專用筆",
+          "img": "assets/img/products/animalLabels/earTagMarkerPen.jpg",
+          "url": "product/animalLabels/earTagMarkerPen.html"
+        },
+        {
+          "id": "electricEarTagApplicator",
+          "name": "電動打耳標器",
+          "img": "assets/img/products/animalLabels/electricEarTagApplicator.jpg",
+          "url": "product/animalLabels/electricEarTagApplicator.html"
+        },
+        {
+          "id": "electronicEarTagReader",
+          "name": "電子耳標讀取器",
+          "img": "assets/img/products/animalLabels/electronicEarTagReader.jpg",
+          "url": "product/animalLabels/electronicEarTagReader.html"
+        },
+        {
+          "id": "markingCrayon",
+          "name": "標記蠟筆",
+          "img": "assets/img/products/animalLabels/markingCrayon.jpg",
+          "url": "product/animalLabels/markingCrayon.html"
+        },
+        {
+          "id": "sectorEarTag",
+          "name": "扇形耳標",
+          "img": "assets/img/products/animalLabels/sectorEarTag.jpg",
+          "url": "product/animalLabels/sectorEarTag.html"
+        },
+        {
+          "id": "sheepEarTag",
+          "name": "羊用耳標",
+          "img": "assets/img/products/animalLabels/sheepEarTag.jpg",
+          "url": "product/animalLabels/sheepEarTag.html"
+        },
+        {
+          "id": "squareEarTag",
+          "name": "方形耳標",
+          "img": "assets/img/products/animalLabels/squareEarTag.jpg",
+          "url": "product/animalLabels/squareEarTag.html"
+        }
+      ]
+    }
+  ]
+}; 
