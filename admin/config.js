b9d71269d728c1972757c2432f4f4009fbf1fafa
Garry <>
2025-12-19 19:10:01 +0800

    換鑰匙

--- admin/config.js
+++ admin/config.js
@@ -3,7 +3,7 @@
 // 此檔案已加入 .gitignore，不會被提交到 Git
 
 const GITHUB_CONFIG = {
-  token: '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',  // 填入你的 GitHub Personal Access Token
+  token: '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',  // 填入你的 GitHub Personal Access Token
   repo: 'Garry85317/Witega',   // 填入你的 Repository（格式：username/repo）
 };
 
